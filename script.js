db3cc7e4a2504c15bb252dab5001385e930b3ef8
elvries <elvries@hotmail.com>
2021-01-15 14:14:25 +0000

    Automatic slideshow

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+/* Start manual slideshow */
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+/* Start automatic slideshow */
+var slideIndex = 0;
+showSlides();
 
 function showSlides(n) {
   var i;
@@ -27,6 +32,15 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-
-
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
 
